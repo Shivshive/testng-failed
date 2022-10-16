b389c7541de3d766fdada26a71686186b05e00ed
= <=>
2022-10-16 16:58:38 +0530

    changed condition to intentional fail

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -10,7 +10,7 @@ public class Test1 {
 	@Test
 	public void test1_sample() {
 		System.out.println("Running Test 1 test1_sample method");
-		AssertJUnit.assertTrue(true);
+		AssertJUnit.assertTrue(false);
 	}
 
 }
