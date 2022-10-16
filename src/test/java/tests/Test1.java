acbae33846cd3ded4ff6f208da4cbc640687c328
= <=>
2022-10-16 16:45:46 +0530

    first commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test1_sample() {
+		System.out.println("Running Test 1 test1_sample method");
+		AssertJUnit.assertTrue(true);
+	}
+
+}
